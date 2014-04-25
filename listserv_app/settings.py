1108a29e1afc24aa38b0346301d7dce1d7dc8f07
wrongu <lange.richard.d@gmail.com>
2014-04-24 21:11:34 -0400

    mailbot is installed. removed submodule.

--- listserv_app/settings.py
+++ listserv_app/settings.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from private import DJANGO_SECRET_KEY
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+from private import DJANGO_SECRET_KEY
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = DJANGO_SECRET_KEY
@@ -48,7 +50,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'listserv_app.urls'
 
 WSGI_APPLICATION = 'listserv_app.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
